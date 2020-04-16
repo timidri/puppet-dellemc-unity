795ecc5ac6a31b994921bf4471ae631eb0f3ea08
Dimitri Tischenko <dimitri@puppet.com>
2020-04-16 16:24:48 +0200

    removed pry requirement

--- tasks/list_jobs.rb
+++ tasks/list_jobs.rb
@@ -1,5 +1,4 @@
 #!/opt/puppetlabs/puppet/bin/ruby
-require 'pry'
 require_relative '../lib/puppet/util/task_helper'
 require_relative '../lib/puppet/type/unity_job'
 task = Puppet::Util::TaskHelper.new('unity')
