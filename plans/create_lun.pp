afc1babcbdc496208dec07f7964009ab7a1c4e5f
Dimitri Tischenko <dimitri@puppet.com>
2020-04-22 18:29:36 +0200

    neatification

--- plans/create_lun.pp
+++ plans/create_lun.pp
@@ -1,16 +1,15 @@
 plan dellemc_unity::create_lun(
-  TargetSpec $targets,
-  String[1] $name,
-  Optional[String] $pool_id = undef,
-  Integer $size,
+  TargetSpec        $targets,
+  String[1]         $name,
+  Optional[String]  $pool_id = undef,
+  Integer           $size,
   Optional[Boolean] $is_thin_enabled = true,
   ) {
     if $pool_id {
       $_pool_id = $_pool_id
     } else {
       $pools = run_task('dellemc_unity::list_pools', $targets).first.value['pools']
-      # notice($pools)
-      notice("pools size: ${pools.size}")
+      out::message("pools size: ${pools.size}")
       if $pools.size == 1 {
         $_pool_id = $pools[0]['content']['id']
         out::message("Found one pool ${_pool_id}; creating LUN on this pool...")
