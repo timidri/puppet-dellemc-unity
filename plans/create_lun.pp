74302f2c23b4a64fdfecca5b1defb325f3d6c6b6
Dimitri Tischenko <dimitri@puppet.com>
2020-04-23 21:31:58 +0200

    change size parameter to accept letters

--- plans/create_lun.pp
+++ plans/create_lun.pp
@@ -2,16 +2,16 @@ plan dellemc_unity::create_lun(
   TargetSpec        $targets,
   String[1]         $name,
   Optional[String]  $pool_id = undef,
-  Integer           $size,
+  Pattern[/[0-9]+[MGT]i?[Bb]$/, /[0-9]+$/] $size,
   Optional[Boolean] $is_thin_enabled = true,
   ) {
     if $pool_id {
       $_pool_id = $_pool_id
     } else {
       $pools = run_task('dellemc_unity::list_pools', $targets).first.value['pools']
-      out::message("pools size: ${pools.size}")
+      # out::message("pools size: ${pools.size}")
       if $pools.size == 1 {
-        $_pool_id = $pools[0]['content']['id']
+        $_pool_id = $pools[0]['id']
         out::message("Found one pool ${_pool_id}; creating LUN on this pool...")
       }
     }
@@ -19,9 +19,18 @@ plan dellemc_unity::create_lun(
   $result = run_task('dellemc_unity::create_lun', $targets, {
     name            => $name,
     pool_id         => $_pool_id,
-    size            => $size,
+    size            => $size.to_bytes,
     is_thin_enabled => $is_thin_enabled,
   })
 
-  notice($result)
+  debug($result)
+
+  if ! $result.ok {
+    Error(
+      message    => "Sorry, this plan does not work yet.",
+      kind       => 'mymodule/error',
+      issue_code => 'NOT_IMPLEMENTED'
+    )
+    fail_plan("Sorry, this plan does not work yet.", 'mymodule/error')
+  }
 }
