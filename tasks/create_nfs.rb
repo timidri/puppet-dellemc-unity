c9359eb50d8e549c2df8c079df59bfff71a06066
Dimitri Tischenko <dimitri@puppet.com>
2020-04-28 13:02:34 +0200

    refactor

--- tasks/create_nfs.rb
+++ tasks/create_nfs.rb
@@ -1,25 +1,25 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 require_relative '../lib/puppet/util/task_helper'
-task = Puppet::Util::TaskHelper.new('unity')
-result = {}
 
+class CreateNfsTask < TaskHelper
 
-begin
-  # Puppet.debug = true 
+  def task(name:, pool_id:, nas_id:, size:, **kwargs)
+    result = {}
+    begin
+      result['response'] = transport.create_nfs(name, pool_id, nas_id, size)
+    rescue Exception => e # rubocop:disable Lint/RescueException
+      result[:_error] = { msg: e.message,
+                          kind: 'timidri-unity/unknown',
+                          details: {
+                            class: e.class.to_s,
+                            backtrace: e.backtrace,
+                          } }
+    end
+    puts result.to_json
+  end
 
-  result['response'] = task.transport.create_nfs(
-    task.params['name'],
-    task.params['pool_id'],
-    task.params['nas_id'],
-    task.params['size'],
-  )
-rescue Exception => e # rubocop:disable Lint/RescueException
-  result[:_error] = { msg: e.message,
-                      kind: 'timidri-unity/unknown',
-                      details: {
-                        class: e.class.to_s,
-                        backtrace: e.backtrace,
-                      } }
+  if __FILE__ == $0
+    CreateNfsTask.run
+  end
+
 end
-
-puts result.to_json
