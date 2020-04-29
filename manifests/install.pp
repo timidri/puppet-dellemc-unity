35b42bda8352a6a641a04fb02fee31274d1a1bc8
Dimitri Tischenko <dimitri@puppet.com>
2020-04-29 17:01:54 +0200

    install rubygems add default spec test

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,15 @@
-# install prerequisites
+# @summary Install prerequisites for the module
+##
+# @example
+#   include dellemc_unity::install
+#
 class dellemc_unity::install() {
+  package { 'rubygems':
+    ensure => present,
+  }
   package { 'rest-client':
     ensure   => 'latest',
     provider => 'gem',
+    require  => Package['rubygems'],
   }
 }
